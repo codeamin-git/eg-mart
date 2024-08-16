882224d2850655593cda537120c646ed78d2cb54
codeamin11 <legacyofalamin@gmail.com>
2024-08-17 01:16:27 +0600

    update: register page built

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import useAuth from '../../hooks/useAuth';
 
 const Register = () => {
+    const { createUser } = useAuth();
+    const navigate = useNavigate();
+    const from = '/';
+
+    const { handleSubmit, register, formState: { errors } } = useForm();
+
+    const onSubmit = (data) => {
+        const { email, password } = data
+        createUser(email, password)
+            .then(result => {
+                console.log(result.user);
+                const newUser = result.user
+                if(newUser){
+                  navigate(from)
+                  toast.success('Signed Up Successfully!')
+                }
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }
+
     return (
-        <div>
-            
+        <div className='w-full md:w-2/3 mx-auto p-2'>
+
         </div>
     );
 };
