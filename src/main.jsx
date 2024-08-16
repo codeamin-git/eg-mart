0716951c843e0967a37d40f7d91f2ab067af35fe
codeamin11 <legacyofalamin@gmail.com>
2024-08-16 23:14:04 +0600

    firebase authprovider added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  RouterProvider
+} from "react-router-dom";
+import { router } from './routes/Routes';
+import { Toaster } from 'react-hot-toast';
+import AuthProvider from './provider/AuthProvider';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <div className='max-w-7xl mx-auto font-poppins'>
+    <AuthProvider>
+    <RouterProvider router={router} />
+    <Toaster />
+    </AuthProvider>
+    </div>
   </StrictMode>,
 )
