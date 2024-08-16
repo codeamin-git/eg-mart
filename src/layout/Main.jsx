943eb58a8616af44984fd1249656b76ca1054aa5
codeamin11 <legacyofalamin@gmail.com>
2024-08-16 23:23:08 +0600

    useAuth hook added for auth

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 const Main = () => {
     return (
         <div>
-            <h2 className="text-4xl">this is home</h2>
+            <Outlet />
         </div>
     );
 };
